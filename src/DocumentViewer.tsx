3196ec43f38cf42c03c6579456342fd7a7def0f0
3imed-jaberi <imed_jebari@hotmail.fr>
2021-04-16 02:21:19 +0200

    add complete simple --related --issue-228

--- src/DocumentViewer.tsx
+++ src/DocumentViewer.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Platform } from "react-native";
-import { DocumentView } from "react-native-pdftron";
+import { DocumentView, Config } from "react-native-pdftron";
 import { DocumentViewerProps } from "../App";
 
 const DocumentViewer = ({ route, navigation }: DocumentViewerProps) => {
   const { document } = route.params;
+
+  const [annotationData, setAnnotationData] = useState('')
+
+  const viewerRef = useRef(null)
+
+  useEffect(() => {
+    if (annotationData) {
+      // console.log(annotationData);
+      
+      const { id, pageNumber, rect: { x1, x2, y1, y2 } } = annotationData
+
+      viewerRef
+        .current
+        .setPropertiesForAnnotation(id, pageNumber, {
+          rect: { x1, x2, y1, y2 },
+          contents: JSON.stringify({ ok: true, msg: 'Hello World !'})
+        })
+        .then(console.log('>>>>>>> success loaded!'))
+        .catch(console.log('>>>>>>> failed loaded!'))
+    }
+  }, [annotationData])
+
   return (
     <DocumentView
+      ref={viewerRef}
       document={document}
       showLeadingNavButton={true}
       leadingNavButtonIcon={
@@ -15,6 +38,19 @@ const DocumentViewer = ({ route, navigation }: DocumentViewerProps) => {
           : "ic_arrow_back_white_24dp"
       }
       onLeadingNavButtonPressed={() => navigation.goBack()}
+      overrideBehavior={[Config.Actions.stickyNoteShowPopUp]}
+      onBehaviorActivated={async ({ action, data }) => {
+        try {
+          if (action === Config.Actions.stickyNoteShowPopUp) {
+            const xfdfCommand = await viewerRef.current.exportAnnotations()
+            if (xfdfCommand.includes('LocationPin')) {
+              setAnnotationData(data)
+            }
+          }
+        } catch (error) {
+          console.log('something worng.');
+        }
+      }}
     />
   );
 };
